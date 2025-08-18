54400a9c12e94d10dda4fab4c903494ae8dd4725
Igor Tavares Rocha <igort10rocha@gmail.com>
2025-08-18 11:25:34 -0300

    nova estrutura, mais facil e organizada

--- core/base_bot.py
+++
@@ -1,8 +0,0 @@
-# Um "contrato" (classe base) que todo bot deve seguir
-
-class BaseBot:
-    def process_message(self, message: str) -> str:
-        raise NotImplementedError("O método 'process_message' deve ser implementado.")
-
-    def send_message(self, message: str) -> None:
-        raise NotImplementedError("O método 'send_message' deve ser implementado.")
