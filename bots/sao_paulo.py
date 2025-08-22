ca5f72c993be842c7a4f678a896f3200b99eb5bc
Igor Tavares Rocha <igort10rocha@gmail.com>
2025-08-22 08:57:44 -0300

    atualizando chormedriver local

--- bots/sao_paulo.py
+++ bots/sao_paulo.py
@@ -1,15 +1,9 @@
 import logging
 import utils.selenium_utils as u
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-servico = ChromeService(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
+navegador = u.getDriver("itbi.prefeitura.sp.gov.br/forms/frm_sql.aspx?tipo=SQL#/")
 
 def sao_paulo_bot():
     logging.info("Iniciando o bot de São Paulo...")
@@ -17,9 +11,11 @@ def sao_paulo_bot():
     try:
         logging.info("Operação do bot em andamento...")
 
-        navegador.get("https://itbi.prefeitura.sp.gov.br/forms/frm_sql.aspx?tipo=SQL#/")
 
-        u.click_button("//*[@id='modalPanelAtencao']/div[2]/div/div[2]/div/div/input[1]", navegador, timeout=10)
+
+        navegador.find_element(By.CLASS_NAME, "cc__button__autorizacao--all").click()
+        time.sleep(10)
+        # u.click_button("//*[@id=\"modalPanelAtencao\"]/div[2]/div/div[2]/div/div/input[1]", navegador, timeout=10)
 
     except Exception as e:
         logging.error(f"Erro no bot de São Paulo: {e}")
